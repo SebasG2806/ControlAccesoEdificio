ad99a37d938ed876de3e6c20f107083771e9156d
SebasG2806 <sebas.sebas1000399@gmail.com>
2025-05-18 18:57:44 -0500

    Conexión singleton

---
+++ ControlAccesoEdificio/Utils/DBConnectionSingleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ControlAccesoEdificio.Utils
+{
+        public sealed class DbConnectionSingleton
+        {
+            private static SqlConnection _conexion;
+            private static readonly object _lock = new object();
+
+            private DbConnectionSingleton() { }
+
+            public static SqlConnection Instancia
+            {
+                get
+                {
+                    if (_conexion == null ||
+                        _conexion.State == System.Data.ConnectionState.Broken ||
+                        _conexion.State == System.Data.ConnectionState.Closed)
+                    {
+                        lock (_lock)
+                        {
+                            if (_conexion == null ||
+                                _conexion.State == System.Data.ConnectionState.Broken ||
+                                _conexion.State == System.Data.ConnectionState.Closed)
+                            {
+                                string cadena = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                                _conexion = new SqlConnection(cadena);
+                            }
+                        }
+                    }
+                    return _conexion;
+                }
+            }
+        }
+    
+}
