a60cbb1c06bdc4201062c99381a003536db8be80
SebasG2806 <sebas.sebas1000399@gmail.com>
2025-05-18 19:04:08 -0500

    Creando la clase empleado

---
+++ ControlAccesoEdificio/Entities/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlAccesoEdificio.Entities
+{
+    public class Empleado
+    {
+        public int EmpleadoId { get; set; }
+        public string Nombre { get; set; }
+        public string Rol { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public int ZonaAcceso { get; set; }
+    }
+}
